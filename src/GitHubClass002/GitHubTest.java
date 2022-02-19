ef7d9ccda5742213846049f52f5968c7f5f4fa86
imran <imran.khattak22@gmail.com>
2022-02-18 20:58:09 -0500

    This commit has all the files and basic cod in src

--- src/GitHubClass002/GitHubTest.java
+++ src/GitHubClass002/GitHubTest.java
@@ -3,7 +3,7 @@ public class GitHubTest {
 
 	public static void main(String[] args) {
 		System.out.println(" This is 1st Git Config");
-		System.out.println(" changing");
+		System.out.println(" changing ");
 
 	}
 
